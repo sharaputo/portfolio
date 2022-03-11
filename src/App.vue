ff873ae436d45895a4324218dbf2f32095df6315
Fedor Sharaputo <sharaputo@icloud.com>
2022-03-11 07:03:13 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,212 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <SmnvHeader />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SmnvHeader from "./components/SmnvHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SmnvHeader,
+  },
+  data() {
+    return {
+      isEnglish: true,
+    };
+  },
+  methods: {
+    switchLanguage() {
+      this.isEnglish = !this.isEnglish;
+    },
   },
 };
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+:focus,
+:active {
+  outline: none;
+}
+::-webkit-file-upload-button {
+  font: inherit;
+  -webkit-appearance: button;
+}
+[role="button"] {
+  cursor: pointer;
+}
+[tabindex="-1"]:focus:not(:focus-visible) {
+  outline: 0 !important;
+}
+a:active,
+a:focus {
+  outline: none;
+}
+a,
+a:hover,
+a:visited {
+  text-decoration: none;
+}
+a,
+button {
+  -webkit-tap-highlight-color: transparent;
+}
+footer,
+header,
+main,
+nav,
+section {
+  display: block;
+}
+button {
+  font-family: inherit;
+  line-height: inherit;
+  text-transform: none;
+  background: transparent;
+  padding: 0;
+  border: none;
+  border-radius: 0;
+  outline: none;
+  padding: 0;
+  margin: 0;
+  overflow: visible;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+}
+button::-moz-focus-inner {
+  padding: 0;
+  border: 0;
+}
+button,
+[type="button"] {
+  -webkit-appearance: button;
+}
+button,
+[type="button"] {
+  &:not(:disabled) {
+    cursor: pointer;
+  }
+}
+button::-moz-focus-inner,
+[type="button"]::-moz-focus-inner {
+  padding: 0;
+  border-style: none;
+}
+img,
+svg {
+  max-width: 100%;
+  height: auto;
+  vertical-align: top;
+}
+img[src$=".svg"] {
+  max-width: none;
+  width: 100%;
+  height: auto;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+html,
+body {
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+p {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+ul {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+ul li {
+  list-style: none;
+}
+
+body {
+  min-width: 320px;
+  font-family: "Inter", sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.3;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  color: #fff;
+  background-color: #201f1f;
+}
+
+.container {
+  width: 100%;
+  padding: 0 15px;
+  margin: 0 auto;
+
+  @media screen and (min-width: 600px) {
+    max-width: 480px;
+    padding: 0;
+  }
+  @media screen and (min-width: 768px) {
+    max-width: 600px;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 100%;
+    padding: 0 15px;
+  }
+  @media screen and (min-width: 1440px) {
+    max-width: 1320px;
+    padding: 0;
+  }
+}
+
+.ibg {
+  position: relative;
+  height: 0;
+  overflow: hidden;
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+}
+
+.fluid-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+
+.hyphenate {
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  -webkit-hyphens: auto;
+  -moz-hyphens: auto;
+  -ms-hyphens: auto;
+  hyphens: auto;
 }
 </style>
---
+++ src/components/SmnvHeader.vue
@@ -0,0 +1,230 @@
+<template>
+  <header class="header" id="top">
+    <div class="container header__container">
+      <div class="header__lang">
+        <button
+          type="button"
+          :class="['switcher', { _active: isEnglish }]"
+          @click="handleLanguage"
+        >
+          En
+        </button>
+        <button
+          type="button"
+          :class="['switcher', { _active: !isEnglish }]"
+          @click="handleLanguage"
+        >
+          Ru
+        </button>
+      </div>
+      <div :class="['header__nav', { _active: menuOpened }]">
+        <nav class="nav">
+          <a href="#portfolio" class="nav__link" @click="handleMenu">
+            <span v-if="isEnglish">Portfolio</span>
+            <span v-else>Портфолио</span>
+          </a>
+          <a href="#service" class="nav__link" @click="handleMenu">
+            <span v-if="isEnglish">Services</span>
+            <span v-else>Услуги</span>
+          </a>
+          <a href="#stages" class="nav__link" @click="handleMenu">
+            <span v-if="isEnglish">Work stages</span>
+            <span v-else>Этапы работы</span>
+          </a>
+          <a href="#contacts" class="nav__link" @click="handleMenu">
+            <span v-if="isEnglish">Contacts</span>
+            <span v-else>Контакты</span>
+          </a>
+        </nav>
+        <div class="social-links">
+          <a
+            href="https://www.instagram.com/websmnv/?hl=ru"
+            class="social-link"
+            target="_blank"
+            @click="handleMenu"
+          >
+            Instagram
+          </a>
+          <a
+            href="tg://resolve?domain=n_smnv"
+            class="social-link"
+            target="_blank"
+            @click="handleMenu"
+          >
+            Telegram
+          </a>
+          <a
+            href="https://www.behance.net/nataliasemenova"
+            class="social-link"
+            target="_blank"
+            @click="handleMenu"
+          >
+            Behance
+          </a>
+        </div>
+      </div>
+      <button
+        type="button"
+        :class="['header__burger', { _active: menuOpened }]"
+        @click="handleMenu"
+      >
+        <span class="line line--top"></span>
+        <span class="line line--bottom"></span>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "SmnvHeader",
+  data() {
+    return {
+      menuOpened: false,
+      isEnglish: true,
+    };
+  },
+  methods: {
+    handleMenu() {
+      this.menuOpened = !this.menuOpened;
+    },
+    handleLanguage() {
+      this.isEnglish = !this.isEnglish;
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.header {
+  padding-top: 30px;
+  &__container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media screen and (min-width: 1200px) {
+      flex-direction: row-reverse;
+      gap: 105px;
+    }
+  }
+  &__lang {
+    position: relative;
+    display: flex;
+    gap: 15px;
+    z-index: 10;
+
+    .switcher {
+      font-size: 14px;
+      line-height: 1;
+      text-transform: uppercase;
+      color: #fff;
+
+      &._active {
+        text-decoration: underline;
+      }
+    }
+  }
+  &__nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &._active {
+      opacity: 1;
+      transform: scale(1);
+    }
+
+    .nav {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      &__link {
+        font-size: 20px;
+        line-height: 24px;
+        text-transform: uppercase;
+        color: #fff;
+        transition: color 0.3s ease-in-out;
+
+        &:hover {
+          color: #a1a1a1;
+        }
+
+        @media screen and (min-width: 1200px) {
+          font-size: 14px;
+          line-height: 17px;
+        }
+      }
+
+      @media screen and (max-width: 1199px) {
+        flex-direction: column;
+      }
+    }
+    .social-links {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 33px;
+
+      @media screen and (min-width: 1200px) {
+        gap: 20px;
+      }
+    }
+    .social-link {
+      font-size: 14px;
+      line-height: 1.3;
+      color: #fff;
+      text-transform: uppercase;
+      text-decoration: underline;
+      transition: color 0.3s ease-in-out;
+
+      &:hover {
+        color: #a1a1a1;
+      }
+    }
+    @media screen and (max-width: 1199px) {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      flex-direction: column;
+      opacity: 0;
+      padding: 160px 15px 30px;
+      transform: scale(0.9);
+      transition: opacity 0.2s ease-in-out, transform 0.3s ease-in-out;
+    }
+    @media screen and (min-width: 1200px) {
+      flex-grow: 1;
+    }
+  }
+  &__burger {
+    position: relative;
+    width: 28px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    z-index: 10;
+
+    .line {
+      width: 100%;
+      height: 2px;
+      background-color: #fff;
+      transform: translateY(0px);
+      transition: transform 0.3s ease-in-out;
+    }
+    &._active {
+      .line--top {
+        transform: translateY(6px) rotate(45deg);
+      }
+      .line--bottom {
+        transform: translateY(-6px) rotate(-45deg);
+      }
+    }
+
+    @media screen and (min-width: 1200px) {
+      display: none;
+    }
+  }
+}
+</style>
